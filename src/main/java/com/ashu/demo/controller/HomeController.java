6949d775baf17e5ed0e2786234ed658f34b2e711
jaguar2014 <emailmulat@gmail.com>
2018-03-07 10:26:36 -0500

    Added astrological zodiac display based on date entered

--- src/main/java/com/ashu/demo/controller/HomeController.java
+++ src/main/java/com/ashu/demo/controller/HomeController.java
@@ -84,7 +84,21 @@ public class HomeController {
 
         String userdate =  request.getParameter("userdate");
 
+
+
+        System.out.println(userdate);
+
         LocalDate localUserDate =  LocalDate.parse(userdate);
+        System.out.println(localUserDate.getDayOfMonth());
+
+
+
+
+        String zodiac = getZodiac(localUserDate);
+
+        System.out.println(zodiac);
+
+        model.addAttribute("zodiac", zodiac);
 
        DayOfWeek dayOfWeek = localUserDate.getDayOfWeek();
 
@@ -144,6 +158,97 @@ public class HomeController {
 
 
 
+    }
+
+    private String getZodiac(LocalDate localUserDate) {
+
+        String sign="";
+//dec 22 to jan 19
+
+        if      ((localUserDate.getMonthValue() == 12 && localUserDate.getDayOfMonth() >= 22 && localUserDate.getDayOfMonth() <= 31)
+                || (localUserDate.getMonthValue() ==  1 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 19)) {
+            System.out.println("Capricorn");
+            sign="Capricorn";
+        }
+//january 20 to february 18
+        else if ((localUserDate.getMonthValue() ==  1 && localUserDate.getDayOfMonth() >= 20 && localUserDate.getDayOfMonth() <= 31) ||
+                (localUserDate.getMonthValue() ==  2 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 18)){
+            System.out.println("Aquarius");
+            sign="Aquarius";
+        }
+        //february 19 to march 20
+
+        else if ((localUserDate.getMonthValue() ==  2 && localUserDate.getDayOfMonth() >= 19 && localUserDate.getDayOfMonth() <= 28)
+                || (localUserDate.getMonthValue() ==  3 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 20)){
+            System.out.println("Pisces");
+            sign="Pisces";
+        }
+        //march 21 to april 19
+
+        else if ((localUserDate.getMonthValue() ==  3 && localUserDate.getDayOfMonth() >= 21 && localUserDate.getDayOfMonth() <= 31)
+                || (localUserDate.getMonthValue() ==  4 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 19)){
+            System.out.println("Aries");
+            sign="Aries";
+        }
+//april 20 to may 20
+        else if ((localUserDate.getMonthValue() ==  4 && localUserDate.getDayOfMonth() >= 20 && localUserDate.getDayOfMonth() <= 30)
+                || (localUserDate.getMonthValue() ==  5 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 20)){
+            System.out.println("Taurus");
+            sign="Taurus";
+        }
+//may 21 to june 20
+        else if ((localUserDate.getMonthValue() ==  5 && localUserDate.getDayOfMonth() >= 21 && localUserDate.getDayOfMonth() <= 31)
+                || (localUserDate.getMonthValue() ==  6 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 20)){
+            System.out.println("Gemini");
+            sign="Gemini";
+        }
+//june 21 to july 22
+        else if ((localUserDate.getMonthValue() ==  6 && localUserDate.getDayOfMonth() >= 21 && localUserDate.getDayOfMonth() <= 30)
+                || (localUserDate.getMonthValue() ==  7 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 22)){
+            System.out.println("Cancer");
+            sign="Cancer";
+        }
+//july 23 to august 22
+        else if ((localUserDate.getMonthValue() ==  7 && localUserDate.getDayOfMonth() >= 23 && localUserDate.getDayOfMonth() <= 31)
+                || (localUserDate.getMonthValue() ==  8 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 22)){
+            System.out.println("Leo");
+            sign="Leo";
+        }
+//august 23 to sept 22
+        else if ((localUserDate.getMonthValue() ==  8 && localUserDate.getDayOfMonth() >= 23 && localUserDate.getDayOfMonth() <= 31)
+                || (localUserDate.getMonthValue() ==  9 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 22)){
+            System.out.println("Virgo");
+            sign="Virgo";
+        }
+//sept 23 to oct 22
+        else if ((localUserDate.getMonthValue() ==  9 && localUserDate.getDayOfMonth() >= 23 && localUserDate.getDayOfMonth() <= 30)
+                || (localUserDate.getMonthValue() == 10 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 22)){
+            System.out.println("Libra");
+            sign="Libra";
+        }
+//oct 23 to nov 21
+        else if ((localUserDate.getMonthValue() == 10 && localUserDate.getDayOfMonth() >= 23 && localUserDate.getDayOfMonth() <= 31)
+                || (localUserDate.getMonthValue() == 11 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 21)){
+            System.out.println("Scorpio");
+            sign="Scorpio";
+        }
+//nov 22 to dec 21
+        else if ((localUserDate.getMonthValue() == 11 && localUserDate.getDayOfMonth() >= 22 && localUserDate.getDayOfMonth() <= 30)
+                || (localUserDate.getMonthValue() == 12 && localUserDate.getDayOfMonth() >= 1 && localUserDate.getDayOfMonth() <= 21)){
+            System.out.println("Sagittarius");
+            sign="Sagittarius";
+        }
+
+        else{
+            sign = "Illegal date";
+        }
+
+
+
+
+
+
+        return sign;
     }
 
     @GetMapping("/login")
